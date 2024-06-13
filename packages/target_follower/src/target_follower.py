bad19b880f0eb700cde8ef97f63b84c280831b1a
Mohnish <mohnish4774.be21@chitkara.edu.in>
2024-06-14 01:16:54 +1000

    tyry

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -2,36 +2,46 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
 
-class Target_Follower:
+class TargetFollower:
+    """
+    A class to control a robot to follow an April tag target.
+    """
     def __init__(self):
-        
-        #Initialize ROS node
+        """
+        Initialize the TargetFollower class.
+        """
+        # Initialize ROS node
         rospy.init_node('target_follower_node', anonymous=True)
 
         # When shutdown signal is received, we run clean_shutdown function
         rospy.on_shutdown(self.clean_shutdown)
-        
-        ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
-        self.cmd_vel_pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/akandb/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
-        ################################################################
 
+        # Initialize publisher and subscriber with your robot's name
+        self.cmd_vel_pub = rospy.Publisher('/shravel/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        self.detection_sub = rospy.Subscriber('/apriltag_detector_node/detections', AprilTagDetectionArray, self.detect_tag, queue_size=1)
         rospy.spin() # Spin forever but listen to message callbacks
 
-    # Apriltag Detection Callback
-    def tag_callback(self, msg):
+    def detect_tag(self, msg):
+        """
+        Callback function for April Tag Detection.
+        Moves the robot based on the detected April tag position.
+        """
         self.move_robot(msg.detections)
- 
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+
     def clean_shutdown(self):
+        """
+        Function called when the node is shutting down.
+        Stops the robot before the node is terminated.
+        """
         rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
 
-    # Sends zero velocity to stop the robot
     def stop_robot(self):
+        """
+        Sends zero velocity to stop the robot.
+        """
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
         cmd_msg.v = 0.0
@@ -39,30 +49,48 @@ class Target_Follower:
         self.cmd_vel_pub.publish(cmd_msg)
 
     def move_robot(self, detections):
-
-        #### YOUR CODE GOES HERE ####
-
+        """
+        Moves the robot based on the detected April tag position.
+        """
         if len(detections) == 0:
+            self.stop_robot()
             return
-
-        x = detections[0].transform.translation.x
-        y = detections[0].transform.translation.y
-        z = detections[0].transform.translation.z
-
-        rospy.loginfo("x,y,z: %f %f %f", x, y, z)
-
-
-        # Publish a velocity
+        
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0.0
-        cmd_msg.omega = 0.0
+        
+        x = detections.transform.translation.x
+        y = detections.transform.translation.y
+        z = detections.transform.translation.z
+        rospy.loginfo("x, y, z: %f, %f, %f", x, y, z)
+        rospy.sleep(1)
+        
+        if z > 0.15:
+            cmd_msg.v = 0.2
+            cmd_msg.omega = 0
+        elif z < 0.10:
+            cmd_msg.v = -0.2
+            cmd_msg.omega = 0
+        elif x > 0.05:
+            cmd_msg.v = 0
+            cmd_msg.omega = -0.4
+        elif x < -0.05:
+            cmd_msg.v = 0
+            cmd_msg.omega = 0.4
+        else:
+            self.stop_robot()
+            return
+        
         self.cmd_vel_pub.publish(cmd_msg)
+        rospy.sleep(0.2)
+        self.stop_robot()
 
-        #############################
-
-if __name__ == '__main__':
+def main():
     try:
-        target_follower = Target_Follower()
+        target_follower = TargetFollower()
     except rospy.ROSInterruptException:
         pass
+
+if __name__ == '__main__':
+    main()
+
